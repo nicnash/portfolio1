55f587c5bb05a06b93186c79222efeeafd852229
Nic Nash <nicnash08@gmail.com>
2016-10-03 18:34:25 -0500

    first

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function loadAbout(){
+	document.querySelector(".js-content").innerHTML = "pop";
+	x = document.querySelector(".js-content");
+	console.log(document.querySelector(".js-content"));
+
+}
+
